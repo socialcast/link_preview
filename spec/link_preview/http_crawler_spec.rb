f8e873a55ee97f8cc1bc0db39d8e59152795fe5f
Michael Andrews <mjands@gmail.com>
2016-01-13 11:25:39 -0800

    Bump Copyright range

--- spec/link_preview/http_crawler_spec.rb
+++ spec/link_preview/http_crawler_spec.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
-# Copyright (c) 2014-2015, VMware, Inc. All Rights Reserved.
+# Copyright (c) 2014-2016, VMware, Inc. All Rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
