4eda9321ec77bb83b55e4f94b19a64b109875df7
Michael Andrews <mjands@gmail.com>
2016-02-25 11:24:53 -0800

    Run rubocop -a on spec

--- spec/link_preview/parser_spec.rb
+++ spec/link_preview/parser_spec.rb
@@ -28,27 +28,25 @@ describe LinkPreview::Parser do
     let(:parser) { LinkPreview::Parser.new(config) }
     let(:response) do
       Faraday::Response.new.tap do |response|
-        allow(response).to receive(:headers).and_return({
-          :'content-disposition' => content_disposition
-        })
+        allow(response).to receive(:headers).and_return('content-disposition': content_disposition)
 
         allow(response).to receive(:url).and_return('http://example.com/image-url.jpg')
       end
     end
 
-    context "when the content-disposition header contains a valid filename" do
+    context 'when the content-disposition header contains a valid filename' do
       let(:content_disposition) { 'inline;filename="image-cd.jpg"' }
       it 'parses the filename from the header' do
         expect(parser.parse_image_file_name(response)).to eq('image-cd.jpg')
       end
     end
-    context "when the content-disposition header does not contain a filename" do
+    context 'when the content-disposition header does not contain a filename' do
       let(:content_disposition) { 'inline;' }
       it 'parses the filename from the url' do
         expect(parser.parse_image_file_name(response)).to eq('image-url.jpg')
       end
     end
-    context "when the content-disposition header contains a blank filename" do
+    context 'when the content-disposition header contains a blank filename' do
       let(:content_disposition) { 'inline;filename=""' }
       it 'parses the filename from the url' do
         expect(parser.parse_image_file_name(response)).to eq('image-url.jpg')
--- spec/support/link_preview/link_preview_stubs.rb
+++ spec/support/link_preview/link_preview_stubs.rb
@@ -2,18 +2,18 @@ module LinkPreviewStubs
   DEFAULT_PROPERTIES = Hash[LinkPreview::Content::PROPERTIES.map { |property| [property, nil] }]
 
   def stub_content_not_found(uri = an_instance_of(String))
-    content = double('content', DEFAULT_PROPERTIES.merge(:found? => false, :source => {}))
+    content = double('content', DEFAULT_PROPERTIES.merge(found?: false, source: {}))
     stub_content_helper uri, content
   end
 
   def stub_content(uri = an_instance_of(String), properties = {})
     nested_properties = Hash.new { |h, k| h[k] = {} }
-    nested_properties.merge!(properties.select { |k, v| v.is_a?(Hash) })
-    simple_properties = properties.reject { |k, v| v.is_a?(Hash) }
+    nested_properties.merge!(properties.select { |_k, v| v.is_a?(Hash) })
+    simple_properties = properties.reject { |_k, v| v.is_a?(Hash) }
 
-    content = double('content', DEFAULT_PROPERTIES.merge(:found? => true).merge(simple_properties))
+    content = double('content', DEFAULT_PROPERTIES.merge(found?: true).merge(simple_properties))
     allow(content).to receive(:sources).and_return(nested_properties)
-    allow(content).to receive(:as_oembed).and_return({ :version => '1.0', :type => 'link' }.merge(nested_properties[:oembed]))
+    allow(content).to receive(:as_oembed).and_return({ version: '1.0', type: 'link' }.merge(nested_properties[:oembed]))
 
     stub_content_helper uri, content
   end
